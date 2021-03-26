557f5033dae1530316ab091efc3f175c847e46d5
Naveen Swami <naveen96swami@gmail.com>
2021-03-26 18:53:10 +0530

    Solved problems on kadane algorithms

--- prefix-sum-and-kadane's-algorithm/com/prefix/sum/and/kadane/algo/service/KadanesAlgoService.java
+++ prefix-sum-and-kadane's-algorithm/com/prefix/sum/and/kadane/algo/service/KadanesAlgoService.java
@@ -1,28 +1,136 @@
 package com.prefix.sum.and.kadane.algo.service;
 
+import java.util.Arrays;
+
 public class KadanesAlgoService {
 	
- 
-	 public void sumOfElementBeteenTwoNumber(int[] arr, int arrSize, int[][] queryArr, int querySize) {
-		 int[] frequencyArr = new int[100001];
-		 int[] cumulativeFrequency = new int[100001];
-		 int sum = 0;
-		 for(int element : arr) {
-				 frequencyArr[element] = frequencyArr[element] +1;
-		 }
-		 for(int i = 0; i < frequencyArr.length ; i++) {
-			 sum += frequencyArr[i];
-			 cumulativeFrequency[i] = sum;
-			 
-		 }
-		 
-		 for(int[] query : queryArr) {
-			 int l = query[0];
-			 int r = query[1];
-			 
-			 System.out.println(cumulativeFrequency[r] - cumulativeFrequency[l -1]);
-			 
-		 }
-		 
-	 }
+	public long maximumProfit(int[][] arr, int year) {
+		int[] yearArr = new int[year+1];
+		long maximumProfit = Integer.MIN_VALUE;
+		
+		for(int[] x : arr) {
+			for(int i = x[0] ; i <= x[1]; i++) {
+				yearArr[i] = yearArr[i] + x[2];
+			}
+		}
+		long sum = 0;
+		for(int i = 1; i <= year; i++) {
+			sum += yearArr[i];
+			if(maximumProfit < sum) {
+			   maximumProfit =sum;	
+			} 
+			if(sum < 0) {
+				sum = 0;
+			}
+		}
+		
+		return maximumProfit;	
+	}
+
+	public void getAllToy(int[] toyCostArr, int[] querys) {
+
+		Arrays.sort(toyCostArr);
+
+		for (int freeToys : querys) {
+			int i = 0;
+			int j = toyCostArr.length - 1;
+			long totalCost = 0;
+			while (i <= j) {
+				totalCost += toyCostArr[i];
+				i++;
+				j = j - freeToys;
+			}
+			System.out.println(totalCost);
+		}
+	}
+
+	public void pairProductOrExtractOrignal(int[] productArr, int size) {
+		int n = (1 + (int) Math.sqrt(1 + 8 * size)) / 2;
+		int[] orignalArr = new int[n];
+
+		// ab = p0, ac= p1.............
+		// orignalArr[0] = sqrt((arr[0] * arr[1]) / arr[n-1])
+		// (ab * ac)/ bc = (P0 * P1 )/ Pn-1
+		// a = sqrt((P0 * P1)/ Pn-1)
+		orignalArr[0] = (int) Math.sqrt((productArr[0] * productArr[1]) / productArr[n - 1]);
+
+		for (int i = 1; i < n; i++) {
+			orignalArr[i] = productArr[i - 1] / orignalArr[0];
+		}
+		System.out.println();
+		displayArr(orignalArr);
+
+	}
+
+	public int equilibriumPointInUnsortedArray(int[] arr) {
+		int left = 0;
+		int right = arr.length - 1;
+		long rightSum = 0;
+		long leftSum = 0;
+
+		while (left < right) {
+
+			if (leftSum < rightSum) {
+				leftSum += arr[left];
+				left++;
+			} else {
+				rightSum += arr[right];
+				right--;
+			}
+
+		}
+		if (leftSum == rightSum) {
+			return left;
+		} else {
+			return -1;
+		}
+
+	}
+
+	public long maxContiguousSum(int[] arr) {
+		long maxContiguosSum = Long.MIN_VALUE;
+		long sum = 0;
+
+		for (int element : arr) {
+			sum += element;
+			if (sum > maxContiguosSum) {
+				maxContiguosSum = sum;
+			} else {
+				sum = 0;
+			}
+
+		}
+
+		return maxContiguosSum;
+	}
+
+	public void sumOfElementBeteenTwoNumber(int[] arr, int arrSize, int[][] queryArr, int querySize) {
+		int[] frequencyArr = new int[100001];
+		int[] cumulativeFrequency = new int[100001];
+		int sum = 0;
+		for (int element : arr) {
+			frequencyArr[element] = frequencyArr[element] + 1;
+		}
+		for (int i = 0; i < frequencyArr.length; i++) {
+			sum += frequencyArr[i];
+			cumulativeFrequency[i] = sum;
+
+		}
+
+		for (int[] query : queryArr) {
+			int l = query[0];
+			int r = query[1];
+
+			System.out.println(cumulativeFrequency[r] - cumulativeFrequency[l - 1]);
+
+		}
+	}
+
+	public void displayArr(int[] arr) {
+		for (int element : arr) {
+			System.out.print(element + " ");
+		}
+		System.out.println();
+	}
+
 }
--- prefix-sum-and-kadane's-algorithm/com/prefix/sum/and/kadane/algo/web/KadanesAlgoController.java
+++ prefix-sum-and-kadane's-algorithm/com/prefix/sum/and/kadane/algo/web/KadanesAlgoController.java
@@ -1,5 +1,8 @@
 package com.prefix.sum.and.kadane.algo.web;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.Scanner;
 
 import com.prefix.sum.and.kadane.algo.service.KadanesAlgoService;
@@ -7,12 +10,75 @@ import com.prefix.sum.and.kadane.algo.service.KadanesAlgoService;
 public class KadanesAlgoController {
 
 	static Scanner scanner = new Scanner(System.in);
+	static BufferedReader bReader = new BufferedReader(new InputStreamReader(System.in));
 	static KadanesAlgoService kadanesAlgoService = new KadanesAlgoService();
 
-	public static void main(String[] args) {
-		
-		
+	public static void main(String[] args) throws IOException {
 
+		// takeTestCase();
+		// takeTestCaseWithBufferedReader();
+		takeInput();
+
+	}
+
+	public static void takeInput() {
+		int x = scanner.nextInt();
+		int size = scanner.nextInt();
+		int[][] arr = twoDArray(size, 3);
+		System.out.println(kadanesAlgoService.maximumProfit(arr, x));
+//		int arr[] = oneDArray(size);
+//		int[] querys = oneDArray(querySize);
+//		kadanesAlgoService.getAllToy(arr, querys);
+		// System.out.println(kadanesAlgoService.equilibriumPointInUnsortedArray(arr));
+	}
+
+	public static int[][] twoDArray(int row, int col) {
+		int[][] arr = new int[row][col];
+
+		for (int i = 0; i < row; i++) {
+			for (int j = 0; j < col; j++) {
+				arr[i][j] = scanner.nextInt();
+			}
+		}
+
+		return arr;
+	}
+
+	public static void takeTestCaseWithBufferedReader() throws IOException {
+		int testCase = Integer.parseInt(bReader.readLine());
+		while (testCase-- > 0) {
+			int size = Integer.parseInt(bReader.readLine());
+			int[] arr = getArrayUsingBufferedReader(size);
+			System.out.println(kadanesAlgoService.maxContiguousSum(arr));
+		}
+	}
+
+	public static int[] getArrayUsingBufferedReader(int size) throws IOException {
+		int[] arr = new int[size];
+		String[] line = bReader.readLine().split(" ");
+		for (int i = 0; i < size; i++) {
+			arr[i] = Integer.parseInt(line[i]);
+		}
+		return arr;
+	}
+
+	public static void takeTestCase() {
+		int testCase = scanner.nextInt();
+		while (testCase-- > 0) {
+
+			int size = scanner.nextInt();
+			int[] arr = oneDArray(size);
+			kadanesAlgoService.pairProductOrExtractOrignal(arr, size);
+			// System.out.println(kadanesAlgoService.maxContiguousSum(arr));
+		}
+	}
+
+	public static int[] oneDArray(int size) {
+		int[] arr = new int[size];
+		for (int i = 0; i < size; i++) {
+			arr[i] = scanner.nextInt();
+		}
+		return arr;
 	}
 
 	public static void sumOfElementBetweenTwoNumber() {
@@ -32,5 +98,4 @@ public class KadanesAlgoController {
 
 		kadanesAlgoService.sumOfElementBeteenTwoNumber(arr, size, queryArr, querySize);
 	}
-
 }

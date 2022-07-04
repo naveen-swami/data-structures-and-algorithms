47ec3fc3633d4b0cba74838094fd977e36462c7e
naveen swami <naveen96swami@gmail.com>
2022-07-04 16:36:09 +0530

    solved min cost of ropes

--- Heap/com/heap/web/HeapController.java
+++ Heap/com/heap/web/HeapController.java
@@ -5,20 +5,24 @@ import java.util.List;
 import java.util.Scanner;
 
 import com.contest.service.DSAContest3Service;
+import com.heap.service.HeapService;
 
 public class HeapController {
 
 	static Scanner scan = new Scanner(System.in);
+	static HeapService heapService = new HeapService();
 
 	public static void main(String[] args) {
-		// int num = scan.nextInt();
+//		int sum = a*b;
+//		 int num = scan.nextInt();
 		// System.out.println(dsaContestNumber4Service.closestPrimeNumber(num));
-		// oneDArrayTest();
+//		oneDArrayTest();
+//		multiTestCases();
 		// twoDArrayTest();
 
-		List<Integer> list = null;
-		list.add(5);
-		System.out.println(list);
+//		List<Integer> list = null;
+//		list.add(5);
+//		System.out.println(list);
 	}
 
 	public static void oneDArrayTest() {
@@ -27,6 +31,8 @@ public class HeapController {
 		for (int i = 0; i < size; i++) {
 			arr[i] = scan.nextInt();
 		}
+
+		heapService.minimumCostOfRopes(arr, size);
 
 	}
 
@@ -38,6 +44,19 @@ public class HeapController {
 				cordinates[i][j] = scan.nextInt();
 			}
 		}
+	}
+
+	public static void multiTestCases() {
+		int testCase = scan.nextInt();
+		while (testCase-- > 0) {
+			int size = scan.nextInt();
+			int[] arr = new int[size];
+			for (int i = 0; i < size; i++) {
+				arr[i] = scan.nextInt();
+			}
+			heapService.minimumCostOfRopes(arr, size);
+		}
+
 	}
 
 }
